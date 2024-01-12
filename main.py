9f5eb747a9e5f74824c5afb04ff42f4473921b95
Tushar Gupta <33971494+tg-93@users.noreply.github.com>
2024-01-11 17:08:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import simple_rnn
 
 # data I/O
-data = open('input.txt', 'r').read() # should be simple plain text file
+data = open('wot1.txt', 'r').read() # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
